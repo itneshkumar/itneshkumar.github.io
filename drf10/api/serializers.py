efd2611d857501232667d27a9de7b07dad40cd48
Itnesh Kumar <74128420+itneshkumar@users.noreply.github.com>
2026-05-07 11:57:53 +0530

    Delete drf10 directory

--- drf10/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from django.forms import models
-from rest_framework import serializers
-from .models import Student
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Student
-        fields=['id','name','roll','city']
